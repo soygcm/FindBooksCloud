84e0b68a14089781de1affd84e053ce6e70b700d
soygcm <gcmelmejor@gmail.com>
2014-10-07 21:33:36 -0600

    cambiar el main

---
+++ cloud/main.js
@@ -0,0 +1,7 @@
+var _ = require('underscore')
+var fb = require('cloud/pattern-order.js')
+var keys = require('cloud/keys.js')
+
+var beforeSaveBook = require('cloud/before-save-book.js')
+var afterSaveBook = require('cloud/after-save-book.js')
+var searchFunction = require('cloud/search.js')
